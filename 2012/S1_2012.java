4d2cb92118e87eddb6ac9ccc2bea8b9189dcaeb1
ArnavPal <68573954+ArnavPal@users.noreply.github.com>
2020-09-06 18:41:29 -0400

    Create S1_2012.java

---
+++ 2012/S1_2012.java
@@ -0,0 +1,13 @@
+// Problem name: Don't pass me the ball!
+// Written by: Arnav Pal
+// Accepted by DMOJ: 50/50, 0.695s, 57.51 MB
+import java.util.*;
+public class S1_2003 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int j = input.nextInt();
+    input.close();
+    int ans = (j-1)*(j-2)*(j-3)/6;
+    System.out.println(ans);
+  }
+}
